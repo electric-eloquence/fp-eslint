4f7ba93e71875fee8a47d7329f85f3a0dd61055a
e2tha-e <ee@electric-eloquence.io>
2016-12-03 14:40:31 -0500

    correct main exeuction

--- eslint~extend.js
+++ eslint~extend.js
@@ -7,9 +7,8 @@ const plugins = require('gulp-load-plugins')();
 
 const utilsGulp = require('../../../gulp/utils');
 
-gulp.task('minify', function () {
+gulp.task('eslint', function () {
   return gulp.src(utilsGulp.pathResolve(srcDir.js) + '/src/**/*.js')
-    .pipe(plugins.uglify())
-    .pipe(plugins.rename({extname: '.min.js'}))
-    .pipe(gulp.dest(utilsGulp.pathResolve(srcDir.js) + '/min'));
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format());
 });
