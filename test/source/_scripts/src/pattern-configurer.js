d06462b829e1459dfc8d7dcd2ec47d9b1b004874
e2tha-e <ee@electric-eloquence.io>
2019-03-20 08:26:34 -0400

    .gitignore update

--- test/source/_scripts/src/pattern-configurer.js
+++
@@ -1,9 +0,0 @@
-if (document.body) {
-  const reloader = document.createElement('script');
-  const l = window.location;
-
-  if (window.portReloader && window.location.protocol !== 'file:') {
-    reloader.setAttribute('src', l.protocol + '//' + l.hostname + ':' + window.portReloader + '/livereload.js');
-    document.body.appendChild(reloader);
-  }
-}
